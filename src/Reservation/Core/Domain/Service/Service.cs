895a4fe3962e1f15dcf86bfebb7987821d645c3e
yousef ramezani <youseframezani68@outlook.com>
2022-12-27 22:06:44 +0330

    add suplier

--- src/Reservation/Core/Domain/Service/Service.cs
+++ src/Reservation/Core/Domain/Service/Service.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Domain.Supplier;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,12 +11,13 @@ namespace Domain.Service
     {
         public int Id { get; set; }
         public string Title { get; set; }
-        public string Interval { get; set; }
         public string BasePrice { get; set; }
         public string Description { get; set; }
         public string SupplierId { get; set; }
         public bool Active { get; set; }
+        public string Interval { get; set; }
         public bool IsInternal { get; set; }
+        public List<SuppliierSeviceConfig> SuppliierSeviceConfigs { get; set; }
 
     }
 }
--- src/Reservation/Core/Domain/Supplier/Supplier.cs
+++ src/Reservation/Core/Domain/Supplier/Supplier.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.Design;
 using System.Linq;
+using System.Runtime.InteropServices;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,13 +12,62 @@ namespace Domain.Supplier
 {
     internal class Supplier
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Mobile { get; set; }
-        public string Email { get; set; }
-        public string Description { get; set; }
+        public GuidId Id { get; set; }
+        public SupplierName Name { get; set; }
+        public Mobile Mobile { get; set; }
+        public Email Email { get; set; }
+        public Description Description { get; set; }
         public bool Active { get; set; }
         public bool IsInternal { get; set; }
-        public string Role { get; set; }
+        public bool IsManager { get; set; }
+        public List<SuppliierSeviceConfig> SuppliierSeviceConfigs { get; set; }
+    }
+    internal class SuppliierSeviceConfig
+    {
+        public Guid Id { get; set; }
+        public int SuplierId { get; set; }
+        public int ServiceId { get; set; }
+        public bool Active { get; set; }
+        public bool IsInternal { get; set; }
+    }
+    public class Id<T>
+    {
+        public T Value { get; set; }
+    }
+    public class GuidId :Id<Guid>
+    {
+
+    }
+    public class Mobile
+    {
+
+    }
+    public class Email
+    {
+
+    }
+    public class Description
+    {
+
+    }
+
+
+    internal record SupplierName(string Name)
+    {
+        public string Name { get; set; }
+        public static SupplierName Create(string name)
+        {
+            if (string.IsNullOrEmpty(name))
+                throw new InvalidValueException("");
+
+            return new SupplierName(name);
+        }
+    }
+
+    internal class InvalidValueException : Exception
+    {
+        public InvalidValueException(string message):base(message)
+        {
+        }
     }
 }
